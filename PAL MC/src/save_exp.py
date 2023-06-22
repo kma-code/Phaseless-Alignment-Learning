0178797faac6621d126942b5b72a6ac8a4bf0f10
kma-code <kevin.max@unibe.ch>
2023-06-22 09:22:06 +0200

    Params for taur search; replace pickel with dill

--- PAL MC/src/save_exp.py
+++ PAL MC/src/save_exp.py
@@ -1,7 +1,7 @@
 import numpy as np
 from microcircuit import *
 import logging
-import pickle
+import dill
 
 logging.basicConfig(format='Train model -- %(levelname)s: %(message)s',
                     level=logging.INFO)
@@ -10,11 +10,11 @@ logging.basicConfig(format='Train model -- %(levelname)s: %(message)s',
 def save(MC_list, name='model', path=None):
 	if path is None:
 		with open(name + '.pkl', 'wb') as output:
-			pickle.dump(MC_list, output, pickle.HIGHEST_PROTOCOL)
+			dill.dump(MC_list, output, dill.HIGHEST_PROTOCOL)
 	else:
 		with open(path + "/" + name + '.pkl', 'wb') as output:
-			pickle.dump(MC_list, output, pickle.HIGHEST_PROTOCOL)
-
+			dill.dump(MC_list, output, dill.HIGHEST_PROTOCOL)
+dill
 def load(file='model.pkl'):
 	with open(file, 'rb') as input:
-		return pickle.load(input)
+		return dill.load(input)
